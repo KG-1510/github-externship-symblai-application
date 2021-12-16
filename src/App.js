1469d116a6e448b87d43276730a8d1854340694f
Kushagra Gupta <guptakushagra15.10@gmail.com>
2021-12-16 13:29:13 +0530

    ✨ Add footer, anchor tags

--- src/App.js
+++ src/App.js
@@ -31,8 +31,10 @@ export default function App() {
         <img width={100} src="https://externship.github.in/orglogo/github.png" alt="github-logo" />
       </div>
       <h1 className="font-bold text-3xl my-4 text-center">GitHub Externship - Symbl.ai Submission - Question 3</h1>
-      <p className="text-center px-2 my-4">The cards below are fetched from this API and <span className="text-2xl font-bold">sorted in arranging order by age</span></p>
-      <p className="text-center italic my-4">(Here, age is considered the ID as given in Example 2)</p>
+      <p className="text-center px-2 my-4">The cards below are fetched from
+        <span className="text-blue-500"><a href="https://randomuser.me/api/?inc=id,name,gender,dob,picture&nat=gb&results=10" target="_blank" rel="noopener noreferrer"> this </a></span>
+        API and <span className="text-2xl font-bold">sorted in arranging order by age</span></p>
+      <p className="text-center italic my-4">(Here, age is considered the ID as given in <span className="text-blue-500"><a href="https://drive.google.com/file/d/1psZ0X7snl2dW6o_PJjDgiYMRlBllfhXP/view" target="_blank" rel="noopener noreferrer">Example 2</a></span>)</p>
       {userData !== [] ? (
         userData.map((item) => {
           return (
@@ -49,6 +51,9 @@ export default function App() {
       ) : (
         <h1 className="text-center">Loading...</h1>
       )}
+      <div className="w-full text-center my-16">
+        Coded with ☕ and Submitted by <span className="text-blue-500"><a href="https://github.com/KG-1510" target="_blank" rel="noopener noreferrer">Kushagra Gupta</a></span>
+      </div>
     </div>
   );
 }
